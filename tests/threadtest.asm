54b8194a06facaa8e6e49046dfc3c46cca3214b2
RobotMan2412 <julian@scheffers.net>
2022-08-14 01:21:41 +0200

    Test thing?

---
+++ tests/threadtest.asm
@@ -0,0 +1,174 @@
+
+	// IRQ vector.
+	.db isr_ignore
+	// NMI vector.
+	.db isr_switch
+	// Entry vector.
+	.db entry
+
+// region tasks
+
+
+	// Dummy ISR that ignores everything.
+	.section ".text"
+isr_ignore:
+	MOV PF, [ST]
+	MOV PC, [ST]
+
+
+yield:
+	// Store flags and disable interrupts.
+	MOV [ST], PF
+	AND PF, 0xfffc
+	// Flow into the ISR context switch thing.
+
+	// ISR that invokes context switching.
+isr_switch:
+	// Store context (registers).
+	// MOV R0, [ST]
+	// MOV R1, [ST]
+	// MOV R2, [ST]
+	// MOV R3, [ST]
+	// // Invoke the (very dumb) scheduler to change the stack register.
+	// MOV.JSR PC, scheduler
+	// // Start, ack. irq, zero timer, enable NMI.
+	// MOV [0xfffe], 0x0d06
+	// Notify.
+	MOV [0xfff6], 0x53
+	MOV [0xfff6], 0x77
+	MOV [0xfff6], 0x0a
+	
+	// // Load other context.
+	// MOV R3, [ST]
+	// MOV R2, [ST]
+	// MOV R1, [ST]
+	// MOV R0, [ST]
+	// Return from interrupt.
+	MOV PF, [ST]
+	MOV PC, [ST]
+
+
+	// A very simple scheduler that toggles between two threads.
+scheduler:
+	// Simple swap.
+	MOV R0, ST
+	MOV ST, [otherthread]
+	MOV [otherthread], R0
+	// Return.
+	MOV PC, [ST]
+
+
+//endregion tasks
+
+
+// region entry
+
+entry:
+	// Disable timer.
+	MOV [0xfffe], 0
+	XOR R0, R0
+	
+	// Set up timer.
+	MOV [0xfffa], R0
+	MOV [0xfffb], R0
+	// Set up limit.
+	MOV [0xfff8], 1000
+	MOV [0xfff9], R0
+	
+	// Create dummy stack for thread A.
+	MOV ST, 0xe000
+	MOV [ST], thread_a
+	MOV [ST], R0
+	MOV [ST], R0
+	MOV [ST], R0
+	MOV [ST], R0
+	MOV [ST], R0
+	
+	// Start, ack. irq, zero timer, enable NMI.
+	MOV [0xfffe], 0x0d06
+	
+	// Enable NMI.
+	MOV PF, 0x0001
+	
+	// We are now thread B, go to that loop.
+	MOV PC, thread_b
+
+// endregion entry
+
+
+// region main
+
+	// Print a message.
+print:
+	MOV PC, .check
+	MOV [ST], R1
+.loop:
+	MOV R1, [R0]
+	MOV [0xfff6], R1
+	INC R0
+.check:
+	CMP1 [R0]
+	MOV R1, [ST]
+	MOV.UGE PC, .loop
+
+
+	// Idle for 4+5*R0 cycles.
+idle:
+	DEC R0
+	MOV.CS PC, idle
+	MOV PC, [ST]
+
+
+	// Thread A loop.
+thread_a:
+	// Set R3 to B to indicate thread.
+	MOV R3, 0xB0B0
+.loop:
+	// Idle for 100.
+	MOV R0, 100
+	MOV.JSR PC, idle
+	// Print message.
+	MOV R0, msg_a
+	MOV.JSR PC, print
+	// Loop back.
+	MOV PC, .loop
+
+
+	// Thread B loop.
+thread_a:
+	// Set R3 to A to indicate thread.
+	MOV R3, 0x0A0A
+.loop:
+	// Idle for 200.
+	MOV R0, 200
+	MOV.JSR PC, idle
+	// Print message.
+	MOV R0, msg_b
+	MOV.JSR PC, print
+	// Loop back.
+	MOV PC, .loop
+
+// endregion main
+
+
+// region data
+
+// Read-only in section .rodata, after .text:
+	.section ".rodata"
+
+// Message for thread A.
+msg_a:
+	.db "Thread A message!\n", 0
+
+// Message for thread B.
+msg_b:
+	.db "Thread B message!\n", 0
+
+// Variables in section .bss, at offset 0x8000:
+	.section ".bss"
+
+// The not-currently-running thread.
+otherthread:
+	.zero 1
+
+// endregion data
