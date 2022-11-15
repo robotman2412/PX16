33e7d6324366c620ba15d8b429b704fdfb233cba
RobotMan2412 <julian@scheffers.net>
2022-11-15 20:26:29 +0100

    Window stub.

---
+++ emulator/src/window.c
@@ -0,0 +1,13 @@
+
+#include "window.h"
+
+Display  *disp;
+int       screen;
+Window    window;
+GC        gc;
+XEvent    event;
+KeySym    key;
+
+void window_event_loop() {
+    
+}
---
+++ emulator/src/window.h
@@ -0,0 +1,12 @@
+
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/Xos.h>
+
+#endif // WINDOW_H
